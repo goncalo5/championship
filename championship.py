2a81ab76c5f6de4eb30919a092f5c932a1d46d31
goncalo <goncalo.s.and@gmail.com>
2017-09-29 15:29:22 +0100

    add swiss system tournament

--- championship.py
+++ championship.py
@@ -2,7 +2,9 @@
 import argparse
 import sys
 import os
+import math
 import random
+from collections import OrderedDict
 
 
 def clear_screen():
@@ -36,9 +38,16 @@ def get_values(args):
 
 def ask(choises):
     skip = 0
-    print "\n\nWhat do you perfer?  ({} to skip)".format(skip)
-    answer = raw_input("1 - {}  vs  2 - {}   ".format(choises[0], choises[1]))
-    answer = int(answer)
+    while True:
+        print "\n\nWhat do you perfer?  ({} to skip)".format(skip)
+        answer = raw_input("1 - {}  vs  2 - {}   ".format(choises[0], choises[1]))
+        if answer in ["q", "quit"]:
+            return answer
+        try:
+            answer = int(answer)
+        except ValueError:
+            print "please select {}, 1 or 2".format(skip)
+            continue
     convert = {1: 1, 2: 0}
     if answer in [skip, 1, 2]:
         return choises[convert[answer]]
@@ -56,7 +65,12 @@ def chose_the_bests(values):
             answer = None
             while not answer:
                 answer = ask([values[i], values[i + 1]])
-            values.remove(answer)
+                print "answer: ", answer
+            if answer in ["q", "quit"]:
+                print "bye"
+                return
+            else:
+                values.remove(answer)
         except IndexError:
             pass
     return values
@@ -80,6 +94,28 @@ def generate_random_matches(values):
 # print generate_random_matches(["a", "b", "c"])
 
 
+def generate_1round_swiss_system_matches(points):
+    points = order_points(points)
+    is_odd = len(points) % 2
+    n = len(points) - is_odd
+    matches = []
+    for i in xrange(0, n, 2):
+        matches.append((points[i][0], points[i + 1][0]))
+    if is_odd:
+        points = add_points(choises=points[-1][0], points=points)
+    return matches
+
+
+def swiss_system_matches(points):
+    # ceil(log2 n) * floor(n / 2)
+    n_rounds = int(math.ceil(math.log(len(points), 2)))
+    for round_i in xrange(n_rounds):
+        matches = generate_1round_swiss_system_matches(points)
+        points = dispute_all_matches(matches, points)
+        points = order_points(points)
+    return points
+
+
 def dispute_one_match(choises):
     skip = 0
     while True:
@@ -99,26 +135,40 @@ def dispute_one_match(choises):
 # print match(["goncalo", "ana"])
 
 
-def add_points(choises, answer, points):
+def add_points(choises, points, answer=None):
+    if len(choises) == 1:  # in the case of odd numbers, it still wins points
+        if isinstance(points, list):
+            points = OrderedDict(points)
+        points[choises] += 3
+        return dict(points)
+    answer = int(answer)
     if answer == 0:
         points[choises[0]] += 1
         points[choises[1]] += 1
     else:
+        if isinstance(points, list):
+            points = OrderedDict(points)
         points[choises[answer - 1]] += 3
-    return points
+    return dict(points)
 
 
 def dispute_all_matches(matches, points):
     for match in matches:
         result = dispute_one_match(match)
-        points = add_points(match, result, points)
+        points = add_points(choises=match, points=points, answer=result)
     return points
 
 
 def order_points(points):
-    return sorted(points.items(), key=lambda x: -x[1])
+    if isinstance(points, dict):
+        points = points.items()
+    # sort keys:
+    points = sorted(points)
+    # return sorted values
+    return sorted(points, key=lambda x: -x[1])
 
 
+# presentation:
 def formated_results(ordered_points, msg="{}   ({} points)"):
     formated = ""
     for key, value in ordered_points:
@@ -126,26 +176,52 @@ def formated_results(ordered_points, msg="{}   ({} points)"):
     return formated
 
 
+def formated_menu(n):
+    msg = "championship ({} matches), swiss system ({} matches), league ({} matches)"
+    n_championship_matches = int(math.factorial(n)/(2 * math.factorial(n - 2)))
+    n_swiss_system_matches = int(math.ceil(math.log(n, 2)) * math.floor(n / 2))
+    n_league_matches = int(n - 1)
+    return msg.format(n_championship_matches, n_swiss_system_matches, n_league_matches)
+
+
+def handle_the_options(option, values, points):
+    if option in ["c", "championship"]:
+        matches = generate_random_matches(values)
+        # print "matches: ", matches
+        points = dispute_all_matches(matches, points)
+        # print "points: ", points
+        points = order_points(points)
+        print formated_results(points)
+    elif option in ["s", "swiss system"]:
+        points = swiss_system_matches(points)
+        print formated_results(points)
+    elif option in ["l", "league"]:
+        while len(values) > 1:
+            random.shuffle(values)
+            values = chose_the_bests(values)
+            # print values
+        print "\n\n", values[0]
+    elif option in ["q", "quit"]:
+        sys.exit(0)
+    else:
+        print "\n\nplease insert:"
+        print "(c) for championship,(s) for swiss system, or (l) for league\n"
+
+
 def run():
     clear_screen()
     args = get_args()
     values = get_values(args)
+    if "" in values:
+        values.remove("")
     # print "values: ", values
-
     points = create_points(values)
     # print "points: ", points
-    matches = generate_random_matches(values)
-    # print "matches: ", matches
-    points = dispute_all_matches(matches, points)
-    # print "points: ", points
-    points = order_points(points)
-    print formated_results(points)
-    # while len(values) > 1:
-    #     random.shuffle(values)
-    #     values = chose_the_bests(values)
-    #     # print values
-    # print "\n\n", values[0]
-
+    n = len(points)
+    while True:
+        print formated_menu(n)
+        option = raw_input("what tournament do you want? ").lower()
+        handle_the_options(option, values, points)
 
 if __name__ == '__main__':
     run()
--- test_championship.py
+++ test_championship.py
@@ -17,6 +17,37 @@ class TestChampionship(unittest.TestCase):
         result = championship.generate_random_matches(self.values)
         self.assertEqual(len(result), len(self.values))
 
+    def test_generate_1round_swiss_system_matches(self):
+        points = {"a": 0, "b": 0, "c": 0}
+        result = championship.generate_1round_swiss_system_matches(points)
+        self.assertEqual(result, [("a", "b")])
+
+        points = {"a": 0, "b": 0, "c": 0, "d": 0}
+        result = championship.generate_1round_swiss_system_matches(points)
+        self.assertEqual(result, [("a", "b"), ("c", "d")])
+
+        points = {"a": 0, "b": 1, "c": 2}
+        result = championship.generate_1round_swiss_system_matches(points)
+        self.assertEqual(result, [("c", "b")])
+
+        points = {"a": 1, "b": 4, "c": 3, "d": 0}
+        result = championship.generate_1round_swiss_system_matches(points)
+        self.assertEqual(result, [("b", "c"), ("a", "d")])
+
+        points = {"a": 0}
+        result = championship.generate_1round_swiss_system_matches(points)
+        self.assertEqual(result, [])
+
+        points = {}
+        result = championship.generate_1round_swiss_system_matches(points)
+        self.assertEqual(result, [])
+
+    # def test_swiss_system_matches(self):
+    #     points = {"a": 0, "b": 0}
+    #     result = championship.swiss_system_matches(points)
+    #     expected = [[("a", 3), ("b", 0)], [("b", 3), ("a", 0)], [("a", 1), ("b", 1)]]
+    #     self.assertIn(result, expected)
+
     # def test_dispute_one_match(self):
     #     result = championship.dispute_one_match(["a", "b"])
     #     self.assertIn(result, [0, 1, 2])
@@ -25,18 +56,37 @@ class TestChampionship(unittest.TestCase):
         choises = ["a", "b"]
         answer = 1
         points = {"a": 0, "b": 0, "c": 3}
-        result = championship.add_points(choises, answer, points)
+        result = championship.add_points(choises, points, answer)
         self.assertEqual(result, {"a": 3, "b": 0, "c": 3})
+
         choises = ["a", "b"]
         answer = 0
         points = {"a": 0, "b": 0, "c": 3}
-        result = championship.add_points(choises, answer, points)
+        result = championship.add_points(choises, points, answer)
         self.assertEqual(result, {"a": 1, "b": 1, "c": 3})
+
         choises = ["a", "c"]
         answer = 2
         points = {"a": 0, "b": 0, "c": 3}
-        result = championship.add_points(choises, answer, points)
+        result = championship.add_points(choises, points, answer)
         self.assertEqual(result, {"a": 0, "b": 0, "c": 6})
+
+        choises = ["a", "c"]
+        answer = "2"
+        points = {"a": 0, "b": 0, "c": 3}
+        result = championship.add_points(choises, points, answer)
+        self.assertEqual(result, {"a": 0, "b": 0, "c": 6})
+
+        choises = ["a", "c"]
+        answer = 2
+        points = [("a", 6), ("b", 3), ("c", 0)]  # ordered
+        result = championship.add_points(choises, points, answer)
+        self.assertEqual(result, {"a": 6, "b": 3, "c": 3})
+
+        choises = "c"
+        points = [("a", 6), ("b", 3), ("c", 0)]  # ordered
+        result = championship.add_points(choises, points, answer)
+        self.assertEqual(result, {"a": 6, "b": 3, "c": 3})
 
     # def test_dispute_all_matches(self):
     #     matches = [("a", "c"), ("b", "c"), ("a", "b")]
@@ -56,9 +106,12 @@ class TestChampionship(unittest.TestCase):
         self.assertEqual(result, [])
 
     def test_formated_results(self):
-        msg = "{} {}"
-        result = championship.formated_results([("c", 6), ("a", 3), ("b", 0)], msg)
+        ordered_points = [("c", 6), ("a", 3), ("b", 0)]
+        result = championship.formated_results(ordered_points, "{} {}")
         self.assertEqual(result, "c 6\na 3\nb 0\n")
+
+        result = championship.formated_results(ordered_points, "{}  ({} pts)")
+        self.assertEqual(result, "c  (6 pts)\na  (3 pts)\nb  (0 pts)\n")
 
 if __name__ == '__main__':
     unittest.main()
